e7bccb1005ddf6dd1aa59ff4682acb207baab47a
RoniPel <roni.rose@gmail.com>
2024-12-03 11:12:44 +0200

    New Project sync

---
+++ FileManagementSystem/FileManagementSystem_Spring/src/main/java/FileSystem/example/FileManagementSystem_Spring/FileManagementSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package FileSystem.example.FileManagementSystem_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementSystemSpringApplication.class, args);
+	}
+
+}
---
+++ FileManagementSystem/FileManagementSystem_Spring/src/test/java/FileSystem/example/FileManagementSystem_Spring/FileManagementSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package FileSystem.example.FileManagementSystem_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementSystemSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
